3c6f4245625ce40c27cfdc125e890db6f256d365
Lei_dream <lei_dream@sina.cn>
2015-05-29 11:35:22 +0800

    添加UIView+Common
    添加UIView+Common

---
+++ UIView+Common.m
@@ -0,0 +1,51 @@
+//
+//  UIView+Common.m
+//  table
+//
+//  Created by lei_dream on 15/5/29.
+//  Copyright (c) 2015年 lei_dream. All rights reserved.
+//
+#import "UIView+Common.h"
+
+@implementation UIView (Common)
+
+- (CGFloat)left
+{
+    return self.frame.origin.x;
+}
+
+- (CGFloat)top
+{
+    return self.frame.origin.y;
+}
+
+- (CGFloat)right
+{
+    return self.frame.origin.x + self.frame.size.width;
+}
+
+- (CGFloat)bottom
+{
+    return self.frame.origin.y + self.frame.size.height;
+}
+
+- (CGFloat)width
+{
+    return self.frame.size.width;
+}
+
+- (CGFloat)height
+{
+    return self.frame.size.height;
+}
+
+- (void)removeAllSubviews
+{
+	while (self.subviews.count) 
+    {
+		UIView *child = self.subviews.lastObject;
+		[child removeFromSuperview];
+	}
+}
+
+@end
